bd6d1eb1a105f7610bb37e7be148c972199b4bfb
Neto test <zms.game.next@icloud.com>
2023-08-13 23:13:12 +0300

    13 august

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 # recipes_open = open('recipes.txt')
 # print(recipes_open.read())
 
-cook_book = {}
+
 
 with open('recipes.txt', 'rt') as recipes_file:
+    cook_book = {}
     for recipes in recipes_file:
         recipes_name = recipes.strip()
         recipes_list = []
         recipes_count = recipes_file.readline()
         for x in range(int(recipes_count)):
-            rcps_count = recipes_file.readline()
-            ingredient_name, quantity, measure = rcps_count.strip().split(' | ')
+            ingredient_name, quantity, measure = recipes_file.readline().strip().split(' | ')
             recipes_list.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
-            cook_book_recipes = {recipes_name: recipes_list}
-        y = recipes_file.readline()
-        cook_book.update(cook_book_recipes)
-print(cook_book)
+        recipes_file.readline()
+        cook_book[recipes_name] = recipes_list
+# print(cook_book)
+
+def get_shop_list_by_dishes(dishes, person):
+    for dish in dishes:
+        for x in cook_book:
+            if x == dish:
+                dish_all_persons = quantity * person
+                print(dish)
+            # else:
+                # print('There is no such dish')
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
 
 
 
