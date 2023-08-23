71fdafaff503e8075143c00f63ab5621f0173de0
Neto test <zms.game.next@icloud.com>
2023-08-23 18:06:36 +0300

    23 august

--- Task3/Task3.py
+++ Task3/Task3.py
@@ -1,26 +1,6 @@
 import os.path
 import os
 
-# with open("1.txt") as f:
-#     len_files = f.readlines()
-#     print(len(len_files))
-#     for index, l in enumerate(f):
-#         print(index, l.strip())
-
-# def len_files(files):
-#     with open(files) as f:
-#         open_files = f.readlines()
-#         files_len = len(open_files)
-#         print(files_len)
-#     print(open_files)
-#
-# len_files('1.txt')
-
-# print(os.path.join(os.getcwd(), '1.txt'))
-# print(os.getcwd())
-
-# sorted_dict = sorted(file_list.__dict__.items(), key=lambda x: x[1])
-
 file_wid = r".txt"
 files_txt = [_ for _ in os.listdir() if _.endswith(file_wid)]
 
@@ -30,10 +10,12 @@ def open_files(files):
         with open(r_files) as r_f:
             o_files = r_f.readlines()
             len_files = len(o_files)
-            for x in files:
+            for x in sorted(files):
                 name_files = x
-                file_list.append({"file": name_files, "len": len_files, "text": o_files})                # sorted_dict = sorted(file_list.__dict__.items(), key=lambda x: x[1])
+                file_list.append({"file": name_files, "len": len_files, "text": o_files})
+    sorted_dict = sorted(file_list[0].values(), key=lambda x: x["len"])
     print(file_list)
+
 
 open_files(files_txt)
 
---
+++ ввв.py
@@ -0,0 +1,12 @@
+x = "Привет , Антон!"
+y = x[::-1]
+print(y)
+
+list_new = [893344, 454555, 454555]
+# x = input("Введи номер!")
+# list_new.append(x)
+y = list_new[:2]
+print(y)
+
+dict = {"Россия": "Москва"}
+
